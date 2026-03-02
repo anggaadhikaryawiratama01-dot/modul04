dd71825fee21d5bd6cb5d2f88f15a3182d876585
Angga Adhikarya Wiratama_015 <anggaadhikaryawiratama01@gmail.com>
2026-03-03 00:57:04 +0700

    Fix controller and seed data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,11 +9,10 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      */
-   public function index()
-{
-    $categories = \App\Models\Category::all();
-    return view('categories.index', compact('categories'));
-}
+    public function index()
+    {
+        //
+    }
 
     /**
      * Show the form for creating a new resource.
