4a784b3f83b30268f8ee9a4139b663caacdf10e3
Angga Adhikarya Wiratama_015 <anggaadhikaryawiratama01@gmail.com>
2026-03-02 22:36:57 +0700

    Update CategoryController dan BookController: tambah fitur CRUD

---
+++ database/migrations/2026_03_02_070337_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('books', function (Blueprint $table) {
+        $table->id();
+        // Menghubungkan ke tabel categories
+        $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+        $table->string('judul');      // Sesuai validasi di controller [cite: 40]
+        $table->string('penulis');    // Sesuai validasi di controller [cite: 41]
+        $table->integer('tahun_terbit'); // Sesuai validasi di controller [cite: 42]
+        $table->integer('stok');      // Sesuai validasi di controller [cite: 43]
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
